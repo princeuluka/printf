c5737dd28e0cd192e710b0422d8394c03d394936
stephenrabbi <stephenodoji@gmail.com>
2022-10-18 10:29:07 -0700

    perc_oper.c

---
+++ perc_oper.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * op_percent - main function
+ * @y: The argument pointer.
+ *
+ * Description: This function prints a percent.
+ *
+ * Return: 1.
+ */
+
+int oper_percent(va_list y)
+{
+	(void)y;
+	_putchar('%');
+	return (1);
+}
